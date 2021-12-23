39cb4b8f60b2cbdda484e523c974b5492f371dde
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-12-23 07:35:01 -0600

    Returning cat description to the console

--- src/App.js
+++ src/App.js
@@ -5,15 +5,16 @@ const catFetch = async () => {
   try {
     const cat = await fetch(
       "https://api.thecatapi.com/v1/breeds/search?q=maine%20coon"
-    );
-    const catJSON = await cat.json();
-    console.log(catJSON[0].description);
+    )
+    const catJSON = await cat.json()
+    console.log(catJSON[0].description)
     return catJSON
   } catch (error) {
-    console.error(error);
+    console.error(error)
   }
 }
 
+catFetch() // calls and logs to console
 
 function App() {
 
@@ -24,7 +25,7 @@ function App() {
       <h1> Cat app</h1>
       <p> {cat}</p>
     </div>
-  );
+  )
 }
 
 export default App
