8d3777f629314c01ce3d218c6f0f52977635fafe
Don Stevenson <dstevenson@harbourfrontcentre.com>
2021-12-26 15:10:44 -0600

    logging cat

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
 
   const [cat, setCat] = useState("No Cats")
   console.log("setcat", setCat)
+  console.log("cat", cat)
   return (
     <div className="App">
       <h1> Cat app </h1>
