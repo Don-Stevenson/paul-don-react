a2fd630e52990b04e904f6757db57c71c9e7d927
PandaComp <pantonio2000@gmail.com>
2022-01-23 20:24:41 -0500

    return cat description, add return no cats found message

--- src/App.js
+++ src/App.js
@@ -1,89 +1,80 @@
-import './App.css'
-import React, { useState } from "react"
-import Results from './components/Results'
-import Search from './components/Search'
-
-const catFetch = async () => {
-  try {
-    const cat = await fetch(
-      "https://api.thecatapi.com/v1/breeds/search?q=maine%20coon"
-    )
-    const catJSON = await cat.json()
-    console.log(catJSON[0].description)
-    return catJSON
-  } catch (error) {
-    console.error(error)
-  }
-}
-
-catFetch() // calls and logs to console below
+import "./App.css";
+import React, { useState } from "react";
+import Results from "./components/Results";
+import Search from "./components/Search";
 
 const getBreeds = () => {
-  return fetch("https://api.thecatapi.com/v1/breeds").then ((response)=> {
-    if(response.status === 200) {
-      return response.json()
-    } else {
-      console.log('error')
-    }
-  }).then((res)=> {
-    res.forEach(element => {
-      console.log(element.name)
+  return fetch("https://api.thecatapi.com/v1/breeds")
+    .then((response) => {
+      if (response.status === 200) {
+        return response.json();
+      } else {
+        throw new Error("whoops");
+      }
+    })
+    .then((res) => {
+      res.forEach((element) => {
+        console.log(element.name);
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
-  })
-}
- getBreeds()
+};
+getBreeds();
 
- 
-// https://api.thecatapi.com/v1/breeds
 function App() {
-
   const [cat, setCat] = useState({
     search: "",
-    results: ""
-  })
-  console.log("setcat =>", setCat)
-  console.log("cat =>", cat)
+    results: "",
+  });
 
   const handleInput = (e) => {
-    const value = e.target.value
+    const value = e.target.value;
     setCat((prevState) => {
       return {
         ...prevState,
-        search: value
-      }
-    })
-  }
+        search: value,
+      };
+    });
+  };
 
   async function search(e) {
     try {
-      if (e.key === 'Enter') {
-        const results = await fetch("https://api.thecatapi.com/v1/breeds/search?q=" + cat.search)
-        const resultsJSON = await results.json()
-        setCat(prevState => {
-          return {
-            ...prevState,
-            results: resultsJSON[0].description
-          }
+      if (e.key === "Enter") {
+        const results = await fetch(
+          "https://api.thecatapi.com/v1/breeds/search?q=" + cat.search
+        );
+        const resultsJSON = await results.json();
+        if (!resultsJSON.length) {
+          setCat((prevState) => {
+            return {
+              ...prevState,
+              results: "No cat found",
+            };
+          });
+        } else {
+          setCat((prevState) => {
+            return {
+              ...prevState,
+              results: resultsJSON[0].description,
+            };
+          });
         }
-
-        )
       }
     } catch (error) {
-      console.error(error)
+      console.error(error);
     }
   }
 
   return (
     <div className="App">
       <h1> Cat app </h1>
-      
+
       <Search handleInput={handleInput} search={search} />
-      <Results
-        results={cat.results}
-      />
-   
+      <Results results={cat.results} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
